6093b40db656b94bf9e80f6b12bc92cc2dbf3df3
Atsunori Saito <sai@yedo.com>
2021-10-12 14:06:33 +0900

    Fixed bumper/emergency switch judge logic.

--- main.cpp
+++ main.cpp
@@ -363,7 +363,7 @@ public:
         }
     }
     bool is_ok() {
-        return fail[0].read() != 0 || fail[1].read() != 0;
+        return fail[0].read() != 0 && fail[1].read() != 0;
     }
     void get_failed_state(bool &v5, bool &v16) {
         v5 = fail[0].read() == 0;
