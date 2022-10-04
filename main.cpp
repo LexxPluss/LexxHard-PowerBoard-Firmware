8f3efd49c7a2f7e223b6c9a83473acb77dcd8f61
Atsunori Saito <sai@yedo.com>
2022-10-04 18:02:15 +0900

    Version 2.0.3.

--- main.cpp
+++ main.cpp
@@ -1061,7 +1061,7 @@ private:
         watchdog.kick();
     }
     void poll_10s() {
-        uint8_t buf[8]{'2', '0', '2'}; // version
+        uint8_t buf[8]{'2', '0', '3'}; // version
         can.send(CANMessage{0x203, buf});
     }
     can_driver can;
